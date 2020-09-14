76dfeb3e02805b3bf9931509959ec630c6005d08
Diego <dagomez2530@gmail.com>
2020-09-14 12:35:12 -0500

    Adding file for task 0

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""returns information about an employee, TODO list progress."""
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    u_id = sys.argv[1]
+    url = "https://jsonplaceholder.typicode.com/todos/?userId={}".format(u_id)
+    url_user = "https://jsonplaceholder.typicode.com/users/{}".format(u_id)
+
+    print("url:", url)
+    response_todo = requests.get(url)
+    response_user = requests.get(url_user)
+
+
+    todo_list = response_todo.json()
+    user_info = response_user.json()
+
+    total_todo = len(todo_list)
+    name = user_info.get("name")
+
+    # completed tasks
+    c = 0
+    for task in todo_list:
+        completed_status = task.get('completed')
+        if completed_status:
+            c += 1
+
+    print("Employee {} is done with tasks({}/{})".format(name, c, total_todo))
+    for task in todo_list:
+        completed_status = task.get('completed')
+        if completed_status:
+            title = task.get("title")
+            print("\t", title)
