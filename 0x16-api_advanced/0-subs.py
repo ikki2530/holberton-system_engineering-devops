3c8d5b1102661466bcc05306ed3e128c10b0ea31
Diego <dagomez2530@gmail.com>
2020-09-17 12:55:10 -0500

    Adding file for task 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,12 +6,13 @@ import requests
 def number_of_subscribers(subreddit):
     """Return number of suscribers"""
     if subreddit:
-        heads = {'User-agent': 'dagomez2530'}
-        url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+        try:
+            heads = {'User-agent': 'dagomez2530'}
+            url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
 
-        response = requests.get(url, headers=heads)
+            response = requests.get(url, headers=heads)
 
-        data_reddit = response.json()
-        return data_reddit['data']['subscribers']
-    else:
-        return 0
+            data_reddit = response.json()
+            return data_reddit['data']['subscribers']
+        except:
+            return 0
