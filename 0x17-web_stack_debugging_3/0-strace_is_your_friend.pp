64be1456a2f00438055e7a1f7940602cd80fe2ef
Diego Gomez <dagomez2530@gmail.com>
2020-09-30 16:45:50 +0000

    Adding file for task 0

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,5 @@
 # Solve the Apache Error 500
 exec {'fix-wordp ress':
     command => "sed -i 's/.phpp/.php/g' /var/www/html/wp-settings.php",
-    path    => '/usr/bin',
+    path    => [ '/bin/', '/sbin/' , '/usr/bin/', '/usr/sbin/' ],
 }
